e5c38e9db068d066f32f51de6289a364b2a65435
feedmeastraycat <feedmeastraycat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-01 10:24:44 +0000

    Tag 0.5.4
    git-svn-id: https://plugins.svn.wordpress.org/wp-editor-widget/tags/0.5.4@941472 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -7,6 +7,16 @@ WPEditorWidget = {
 	 * @var string
 	 */
 	currentContentId: '',
+	
+	/**
+	 * @var string
+	 */
+	 currentEditorPage: '',
+	 
+	 /**
+	  * @var int
+	  */
+	 wpFullOverlayOriginalZIndex: 0,
 
 	/**
 	 * Show the editor
@@ -17,6 +27,12 @@ WPEditorWidget = {
 		jQuery('#wp-editor-widget-container').show();
 		
 		this.currentContentId = contentId;
+		this.currentEditorPage = ( jQuery('body').hasClass('wp-customizer') ? 'wp-customizer':'wp-widgets');
+		
+		if (this.currentEditorPage == "wp-customizer") {
+			this.wpFullOverlayOriginalZIndex = parseInt(jQuery('.wp-full-overlay').css('zIndex'));
+			jQuery('.wp-full-overlay').css({ zIndex: 49000 });
+		}
 		
 		this.setEditorContent(contentId);
 	},
@@ -27,33 +43,52 @@ WPEditorWidget = {
 	hideEditor: function() {
 		jQuery('#wp-editor-widget-backdrop').hide();
 		jQuery('#wp-editor-widget-container').hide();
+		
+		if (this.currentEditorPage == "wp-customizer") {
+			jQuery('.wp-full-overlay').css({ zIndex: this.wpFullOverlayOriginalZIndex });
+		}
 	},
 	
 	/**
 	 * Set editor content
 	 */
 	setEditorContent: function(contentId) {
-		var editor = tinyMCE.EditorManager.get('wp-editor-widget');
-		if (typeof editor == "undefined") {
-			jQuery('#wp-editor-widget').val(jQuery('#'+ contentId).val());
+		var editor = tinyMCE.EditorManager.get('wpeditorwidget');
+		var content = jQuery('#'+ contentId).val();
+
+		if (typeof editor == "object" && editor !== null) {
+			editor.setContent(content);
 		}
-		else {
-			editor.setContent(jQuery('#'+ contentId).val());
-		}
+		jQuery('#wpeditorwidget').val(content);
 	},
 	
 	/**
 	 * Update widget and close the editor
 	 */
 	updateWidgetAndCloseEditor: function() {
-		var editor = tinyMCE.EditorManager.get('wp-editor-widget');
-		if (typeof editor == "undefined") {
-			jQuery('#'+ this.currentContentId).val(jQuery('#wp-editor-widget').val());
+		var editor = tinyMCE.EditorManager.get('wpeditorwidget');
+
+		if (typeof editor == "undefined" || editor == null || editor.isHidden()) {
+			var content = jQuery('#wpeditorwidget').val();
 		}
 		else {
-			jQuery('#'+ this.currentContentId).val(editor.getContent());
+			var content = editor.getContent();
 		}
-		wpWidgets.save(jQuery('#'+ this.currentContentId).closest('div.widget'), 0, 1, 0);
+
+		jQuery('#'+ this.currentContentId).val(content);
+		
+		// customize.php
+		if (this.currentEditorPage == "wp-customizer") {
+			var widget_id = jQuery('#'+ this.currentContentId).closest('div.form').find('input.widget-id').val();
+			var widget_form_control = wp.customize.Widgets.getWidgetFormControlForWidget( widget_id )
+			widget_form_control.updateWidget();
+		}
+		
+		// widgets.php
+		else {
+			wpWidgets.save(jQuery('#'+ this.currentContentId).closest('div.widget'), 0, 1, 0);	
+		}
+		
 		this.hideEditor();
 	}
 	
